7deaf99d3091b259317e39d6f3b69ff291cf27af
swag mode activate!!!! <nicholas.kennedy2@ucalgary.ca>
2025-02-16 18:13:04 -0700

    Update application.py

--- application.py
+++ application.py
@@ -3,6 +3,8 @@ from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
@@ -20,6 +22,29 @@ Session(app)
 engine = create_engine(DATABASE_URL)
 db = scoped_session(sessionmaker(bind=engine))
 print('flask is starting')
+
+class Book(db.Model):
+    __tablename__="books"
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.Integer, nullable = False)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+class User(db.Model):
+    __tablename__="users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey("books.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    review = db.Column(db.String, nullable=False)
+
+
 
 @app.route("/")
 def index():
